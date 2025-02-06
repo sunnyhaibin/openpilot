404ad228a9baaad06bb5e98838e6c049ef81792a
rav4kumar <meetkumardesai@gmail.com>
2025-02-05 17:47:44 -0700

    still too dark for my tint

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -174,7 +174,7 @@ void Device::updateBrightness(const UIState &s) {
     }
 
     // Scale back to 5% to 100%
-    clipped_brightness = std::clamp(100.0f * clipped_brightness, 5.0f, 100.0f);
+    clipped_brightness = std::clamp(100.0f * clipped_brightness, 2.0f, 100.0f);
   }
 
   int brightness = brightness_filter.update(clipped_brightness);
